52a2747ddadd3484b43ff0ecd885252f25cad616
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-04-20 17:10:57 -0400

    commented out bonus requirement functions

--- employeemanagementsystem.js
+++ employeemanagementsystem.js
@@ -243,118 +243,121 @@ const updateEmployeeRoles = () => {
 })
 }
 
+// Bonus requirements for updating, view and delete
 
-//Update Employee Manager Function
 
-const updateEmployeeManager = () => {
-    connection.query("SELECT * FROM employee manager", (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        var employees = data.map(e => ({
-            name: `${e.first_name} ${e.last_name}`,
-            value: `${e.id}`
-        }));
-        console.log(managers);
+
+// //Update Employee Manager Function
+
+// const updateEmployeeManager = () => {
+//     connection.query("SELECT * FROM employee manager", (err, data) => {
+//         if (err) throw err;
+//         console.log(data);
+//         var employees = data.map(e => ({
+//             name: `${e.first_name} ${e.last_name}`,
+//             value: `${e.id}`
+//         }));
+//         console.log(managers);
     
-    connection.query("SELECT * FROM manager role", (err, data) => {
-        if (err) throw err;
-        console.log(data);
-        var roles = data.map(r => ({
-            name: `${r.title}`,
-            value: `${r.id}`
-        }));
+//     connection.query("SELECT * FROM manager role", (err, data) => {
+//         if (err) throw err;
+//         console.log(data);
+//         var roles = data.map(r => ({
+//             name: `${r.title}`,
+//             value: `${r.id}`
+//         }));
     
     
-    inquirer
-    .prompt([
-    {
-        name: `updatedManager`,
-        type: `list`,
-        message: `Which manager's role needs to be updated?`,
-        choices: managers
-    }, 
-    {
-        name: `updatedRole`,
-        type: `list`,
-        message: `What is the new role?`,
-        choices: roles
-    }
-])
-    .then(function({updatedManager, updatedRole}) {
-        const query = `UPDATE manager SET role_id = ? WHERE id = ?`;
-        connection.query(query, [updatedManager, updatedRole], function(err, res){
-            if (err) throw err;
-            console.log("Managers and Roles updated");
-            runSearch(); 
-        })
-    })
-});
-})
-}
+//     inquirer
+//     .prompt([
+//     {
+//         name: `updatedManager`,
+//         type: `list`,
+//         message: `Which manager's role needs to be updated?`,
+//         choices: managers
+//     }, 
+//     {
+//         name: `updatedRole`,
+//         type: `list`,
+//         message: `What is the new role?`,
+//         choices: roles
+//     }
+// ])
+//     .then(function({updatedManager, updatedRole}) {
+//         const query = `UPDATE manager SET role_id = ? WHERE id = ?`;
+//         connection.query(query, [updatedManager, updatedRole], function(err, res){
+//             if (err) throw err;
+//             console.log("Managers and Roles updated");
+//             runSearch(); 
+//         })
+//     })
+// });
+// })
+// }
 
-//View Employee by Manager Function
+// //View Employee by Manager Function
 
-const viewEmployeeByManager = () => {
-    inquirer
-    .prompt({
-        name: `view employee by manager`,
-        type: `input`,
-        message: `View employee by manager`,
-    })
-    .then((answer) => {
+// const viewEmployeeByManager = () => {
+//     inquirer
+//     .prompt({
+//         name: `view employee by manager`,
+//         type: `input`,
+//         message: `View employee by manager`,
+//     })
+//     .then((answer) => {
 
-    })
-}
+//     })
+// }
 
-//Delete Department Function
+// //Delete Department Function
 
-const deleteDepartment = () => {
-    inquirer
-    .prompt({
-        name: `department`,
-        type: `input`,
-        message: `Which department would you like to delete?`,
-    })
-    .then((answer) => {
+// const deleteDepartment = () => {
+//     inquirer
+//     .prompt({
+//         name: `department`,
+//         type: `input`,
+//         message: `Which department would you like to delete?`,
+//     })
+//     .then((answer) => {
 
-    })
-}
+//     })
+// }
 
-//Delete Role Function
+// //Delete Role Function
 
-const deleteRole = () => {
-    inquirer
-    .prompt({
-        name: `role`,
-        type: `input`,
-        message: `Which role would you like to delete?`,
-    })
-    .then((answer) => {
+// const deleteRole = () => {
+//     inquirer
+//     .prompt({
+//         name: `role`,
+//         type: `input`,
+//         message: `Which role would you like to delete?`,
+//     })
+//     .then((answer) => {
 
-    })
-}
+//     })
+// }
 
-//Delete Employee Function
+// //Delete Employee Function
 
-const deleteEmployee = () => {
-    inquirer
-    .prompt({
-        name: `employee`,
-        type: `input`,
-        message: `Which employee would you like to delete?`,
-    })
-}
+// const deleteEmployee = () => {
+//     inquirer
+//     .prompt({
+//         name: `employee`,
+//         type: `input`,
+//         message: `Which employee would you like to delete?`,
+//     })
+// }
 
-//View Total Utilized Budget Function
+// //View Total Utilized Budget Function
 
-const viewBudget = () => {
-    inquirer
-    .prompt({
-        name: `view the total utilized budget of a department`,
-        type: `input`,
-        message: `view the total utilized budget of a department`,
-    })
-    .then((answer) => {
+// const viewBudget = () => {
+//     inquirer
+//     .prompt({
+//         name: `view the total utilized budget of a department`,
+//         type: `input`,
+//         message: `view the total utilized budget of a department`,
+//     })
+//     .then((answer) => {
 
-    })
-}
+//     })
+// }
